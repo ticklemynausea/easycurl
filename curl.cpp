8accdd20532004569134e73c7efb351a4735027a
ticklemynausea <emailoftheyear@gmail.com>
2012-12-15 11:47:13 +0000

    Removed main() function used for testing from curl.cpp

--- curl.cpp
+++ curl.cpp
@@ -193,24 +193,3 @@ int EasyCurl::curlRequest() {
 
   return 0;
 }
-
-int main(int argc, char* argv[]) {
-
-  if (argc < 2)
-    return 0;
-    
-  EasyCurl* c = new EasyCurl(argv[1]);
-  
-  if (!c->requestWentOk)
-    cout << c->error_message << "--" << endl;
-    
-  cout << c->request_url << endl;
-  cout << c->response_content_type << endl;
-  cout << c->response_content_length << endl;
-  
-  if (c->isHtml) {
-     cout << "--" << endl << c->response_body << "--" << endl;
-     cout << "Title: " << c->html_title << endl;
-  }
-  return 0;
-}
